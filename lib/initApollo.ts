9f8836208ed5aed85a0687907ca8604f3aa83a19
Ben Awad <benawad97@gmail.com>
2019-01-27 18:51:55 -0600

    apollo setup

---
+++ lib/initApollo.ts
@@ -0,0 +1,60 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  NormalizedCacheObject
+} from "apollo-boost";
+import { setContext } from "apollo-link-context";
+import { createHttpLink } from "apollo-link-http";
+import fetch from "isomorphic-unfetch";
+import { isBrowser } from "./isBrowser";
+
+let apolloClient: ApolloClient<NormalizedCacheObject> | null = null;
+
+// Polyfill fetch() on the server (used by apollo-client)
+if (!isBrowser) {
+  (global as any).fetch = fetch;
+}
+
+interface Options {
+  getToken: () => string;
+}
+
+function create(initialState: any, { getToken }: Options) {
+  const httpLink = createHttpLink({
+    uri: "http://localhost:4000/graphql",
+    credentials: "include"
+  });
+
+  const authLink = setContext((_, { headers }) => {
+    const token = getToken();
+    return {
+      headers: {
+        ...headers,
+        cookie: token ? `qid=${token}` : ""
+      }
+    };
+  });
+
+  // Check out https://github.com/zeit/next.js/pull/4611 if you want to use the AWSAppSyncClient
+  return new ApolloClient({
+    connectToDevTools: isBrowser,
+    ssrMode: !isBrowser, // Disables forceFetch on the server (so queries are only run once)
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache().restore(initialState || {})
+  });
+}
+
+export default function initApollo(initialState: any, options: Options) {
+  // Make sure to create a new client for every server-side request so that data
+  // isn't shared between connections (which would be bad)
+  if (!isBrowser) {
+    return create(initialState, options);
+  }
+
+  // Reuse client on the client-side
+  if (!apolloClient) {
+    apolloClient = create(initialState, options);
+  }
+
+  return apolloClient;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,44 @@
-import Link from 'next/link';
-import * as React from 'react';
-import Layout from '../components/Layout';
+import { gql } from "apollo-boost";
+import Link from "next/link";
+import * as React from "react";
+import { Mutation } from "react-apollo";
+import Layout from "../components/Layout";
 
 const IndexPage: React.FunctionComponent = () => {
   return (
     <Layout title="Home | Next.js + TypeScript Example">
       <h1>hello Next.js 👋</h1>
-      <p><Link href='/about'><a>About</a></Link></p>
+      <p>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </p>
+      <Mutation
+        mutation={gql`
+          mutation {
+            login(email: "test@test.com", password: "qqq") {
+              id
+              firstName
+              lastName
+              email
+              name
+            }
+          }
+        `}
+      >
+        {mutate => (
+          <button
+            onClick={async () => {
+              const response = await mutate();
+              console.log(response);
+            }}
+          >
+            call login mutation
+          </button>
+        )}
+      </Mutation>
     </Layout>
-  )
-}
+  );
+};
 
 export default IndexPage;
